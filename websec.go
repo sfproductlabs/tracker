a7baca79be37e4d0fb8e544be8df68a403980192
dioptre <dioptre@gmail.com>
2018-10-01 23:18:07 -0700

    wip

--- websec.go
+++ websec.go
@@ -193,7 +193,7 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
-	if err := tmpl.ExecuteTemplate(w, "default", data); err != nil {
+	if err := tmpl.ExecuteTemplate(w, "default.tmpl", data); err != nil {
 		log.Println(err.Error())
 		http.Error(w, http.StatusText(500), 500)
 	}
