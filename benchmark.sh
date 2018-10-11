4cab46a284aa89e5be09cca9a5e9cd0db25da562
dioptre <dioptre@gmail.com>
2018-10-10 20:36:38 -0700

    updated simple benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -3,5 +3,5 @@ git submodule update --init --recursive
 git submodule update --recursive --remote
 cd ./tests/wrk
 make
-./wrk -t2 -c100 -d2s $1
+./wrk -t2 -c100 -d2s $1/tr/v1/vid/aFccafd/ROCK/ON/lat/37.232332/lon/6.32233223/first/true/score/6/ref/andy
 cd ../..
