7348146704243d362f9a4a4571d01467e3e955e2
dioptre <dioptre@gmail.com>
2018-11-05 13:53:29 -0800

    added server tracking

--- utils.go
+++ utils.go
@@ -100,11 +100,9 @@ func cacheDir() (dir string) {
 }
 
 func getIP(r *http.Request) string {
-
 	ip := r.Header.Get("X-Forwarded-For")
 	if ip == "" {
 		ip, _, _ = net.SplitHostPort(r.RemoteAddr)
 	}
-	fmt.Println(ip)
 	return ip
 }
