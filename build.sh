d64ea53474c936ae0a2127d9e4e63c39dd3d4d94
Andrew Grosser <dioptre@gmail.com>
2020-08-06 15:44:34 -0700

    wip

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 go get github.com/sfproductlabs/tracker
 go install github.com/sfproductlabs/tracker
+rm -rf pdb && rm -rf pdbwal && mkdir pdb && mkdir pdbwal && echo "*" >> pdb/.gitignore && echo "*" >> pdbwal/.gitignore && rm -rf tmp && mkdir tmp && echo "*" >> tmp/.gitignore
 go build
 sudo docker build -t tracker .
