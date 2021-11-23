310a4d0d277fbe59dced59c18ccc031e243f8a71
Andrew Grosser <dioptre@gmail.com>
2021-11-22 18:01:27 -0800

    links to updates

--- .setup/parcel/src/index.jsx
+++ .setup/parcel/src/index.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useRef, useEffect } from "react";
 import ReactDOM, { createPortal } from "react-dom";
 import track from "./lib/tracker/track"
-
+//Facebook CAPI Info
+//Manage https://business.facebook.com/events_manager2
+//Events listed at https://business.facebook.com/business/help/402791146561655?id=1205376682832142
+//Parameters listed at https://developers.facebook.com/docs/marketing-api/conversions-api/parameters
 const App = () => {
   useEffect(() => {
     track({test_event_code : "TEST76022", ename: "TestEvent", fbx: true, cflags: 4096, action_source : "website", value: 34.4})
